39a33195632b31cae3ac9d3a948740fea3ac695b
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-01-22 23:58:24 +0530

    Implemented the decoder logic using the enable signal.

--- qn8/decoder3_8.v
+++ qn8/decoder3_8.v
@@ -1,14 +1,26 @@
-module decoder3_8(I,Y);
+module decoder3_8(I,en,Y);
 input[2:0] I;
+input en;
 output[7:0] Y;
 
-and a1(Y[0],~I[2],~I[1],~I[0]);
-and a2(Y[1],~I[2],~I[1],I[0]);
-and a3(Y[2],~I[2],I[1],~I[0]);
-and a4(Y[3],~I[2],I[1],I[0]);
-and a5(Y[4],I[2],~I[1],~I[0]);
-and a6(Y[5],I[2],~I[1],I[0]);
-and a7(Y[6],I[2],I[1],~I[0]);
-and a8(Y[7],I[2],I[1],I[0]);
+// decoder logic without using enable pin
+// and a1(Y[0],~I[2],~I[1],~I[0]);
+// and a2(Y[1],~I[2],~I[1],I[0]);
+// and a3(Y[2],~I[2],I[1],~I[0]);
+// and a4(Y[3],~I[2],I[1],I[0]);
+// and a5(Y[4],I[2],~I[1],~I[0]);
+// and a6(Y[5],I[2],~I[1],I[0]);
+// and a7(Y[6],I[2],I[1],~I[0]);
+// and a8(Y[7],I[2],I[1],I[0]);
+
+// decoder logic  using enable pin
+and a1(Y[0],~I[2],~I[1],~I[0],en);
+and a2(Y[1],~I[2],~I[1],I[0],en);
+and a3(Y[2],~I[2],I[1],~I[0],en);
+and a4(Y[3],~I[2],I[1],I[0],en);
+and a5(Y[4],I[2],~I[1],~I[0],en);
+and a6(Y[5],I[2],~I[1],I[0],en);
+and a7(Y[6],I[2],I[1],~I[0],en);
+and a8(Y[7],I[2],I[1],I[0],en);
 
 endmodule
--- qn8/decoder_tb.v
+++ qn8/decoder_tb.v
@@ -1,20 +1,23 @@
 module decoder_tb;
-reg[2:0] I;
+reg[2:0] I; 
+reg en;
 wire[7:0] Y;
 
-decoder3_8 DUT(I,Y);
+decoder3_8 DUT(I,en,Y);
 
 initial
 begin
   $dumpfile("decoder.vcd");
   $dumpvars(0,decoder_tb);
-  $monitor($time," Input = %b | Output = %b",I,Y);
-  I = 3'd7;
+  $monitor($time," Input = %b Enable = %b | Output = %b",I,en,Y);
+  I = 3'd7; en=0;
+  #10 I = 3'd7; en=1;
   #10 I=3'd6;
   #10 I=3'd5;
   #10 I=3'd4;
   #10 I=3'd3;
-  #10 I=3'd2;
+  #10 en=0;
+  #10 I=3'd2; en=1;
   #10 I=3'd1;
   #10 I=3'd0;
   #10 $finish;
